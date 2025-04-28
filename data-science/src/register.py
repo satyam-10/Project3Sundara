8b082dd0a0bedde6209ac130b6f126a042049d42
Satyam kumar <95583751+satyam-10@users.noreply.github.com>
2025-04-28 16:45:30 +0530

    Update register.py

--- data-science/src/register.py
+++ data-science/src/register.py
@@ -14,9 +14,9 @@ def parse_args():
     '''Parse input arguments'''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, help='Name under which model will be registered')  # Hint: Specify the type for model_name (str)
-    parser.add_argument('--model_path', type=str, help='Model directory')  # Hint: Specify the type for model_path (str)
-    parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")  # Hint: Specify the type for model_info_output_path (str)
+    parser.add_argument('--model_name', type=str, help='Name under which model will be registered')
+    parser.add_argument('--model_path', type=str, help='Model directory')
+    parser.add_argument("--model_info_output_path", type=str, help="Path to write model info JSON")
     args, _ = parser.parse_known_args()
     print(f'Arguments: {args}')
 
@@ -27,24 +27,22 @@ def main(args):
 
     print("Registering ", args.model_name)
 
+    # Load model
+    model = mlflow.sklearn.load_model(args.model_path)
 
-    # -----------  WRITE YOR CODE HERE -----------
-    
-    # Step 1: Load the model from the specified path using `mlflow.sklearn.load_model` for further processing.  
-    model = mlflow.sklearn.load_model(args.model)
-    
-    # Step 2: Log the loaded model in MLflow with the specified model name for versioning and tracking. 
+    # Log model using mlflow
+    mlflow.sklearn.log_model(model, args.model_name)
 
-    # Step 3: Register the logged model using its URI and model name, and retrieve its registered version.  
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        registered_model_name="price_prediction_model",  # Descriptive model name for registration
-        artifact_path="random_forest_price_regressor"  # Path to store model artifacts
-    )
-    # Step 4: Write model registration details, including model name and version, into a JSON file in the specified output path.  
+    # Register logged model using mlflow
+    run_id = mlflow.active_run().info.run_id
+    model_uri = f'runs:/{run_id}/{args.model_name}'
+    mlflow_model = mlflow.register_model(model_uri, args.model_name)
+    model_version = mlflow_model.version
+
+    # Write model info
     print("Writing JSON")
     model_info = {"id": f"{args.model_name}:{model_version}"}
-    output_path = os.path.join(args.model_info_output_path,"model_info.json")
+    output_path = os.path.join(args.model_info_output_path, "model_info.json")
     with open(output_path, "w") as of:
         json.dump(model_info, of)
 
